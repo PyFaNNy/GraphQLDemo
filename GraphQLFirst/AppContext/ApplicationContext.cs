928df9ca6f5de1e47ab857b4e50e80861b4c88b3
PyFaNNy <fanny.koob@gmail.com>
2023-01-31 20:48:32 +0300

    Initial appContext

---
+++ GraphQLFirst/AppContext/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using GraphQLFirst.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLFirst.AppContext;
+
+public class ApplicationContext : DbContext, IApplicationContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> dbContextOptions) : base(dbContextOptions)
+    {
+    }
+    
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
+}
---
+++ GraphQLFirst/AppContext/DependenciesInjection.cs
@@ -0,0 +1,24 @@
+﻿using GraphQLFirst.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLFirst.AppContext;
+
+public static class DependenciesBootstrapper
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationContext>(options =>
+        {
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName));
+
+            options.EnableDetailedErrors();
+            options.EnableSensitiveDataLogging();
+        });
+
+        services.AddScoped<IApplicationContext>(provider => provider.GetService<ApplicationContext>());
+
+        return services;
+    }
+}
