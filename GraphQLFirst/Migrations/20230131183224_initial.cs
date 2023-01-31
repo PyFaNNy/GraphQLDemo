9195fe0656222c72c50c2ec1948ab3455af5147a
PyFaNNy <fanny.koob@gmail.com>
2023-01-31 21:35:16 +0300

    Migrations

---
+++ GraphQLFirst/Migrations/20230131183224_initial.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GraphQLFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Owners_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Owners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Owners",
+                columns: new[] { "Id", "Address", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("f9f88d26-3ac9-4248-adb3-754ddb37a811"), "John Doe's address", "John Doe" },
+                    { new Guid("f9f88d26-3ac9-4248-adb3-754ddb37a812"), "Jane Doe's address", "Jane Doe" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "Id", "Description", "OwnerId", "Type" },
+                values: new object[,]
+                {
+                    { new Guid("a20ec81c-95bd-4ade-9432-74528744690d"), "Income account for our users", new Guid("f9f88d26-3ac9-4248-adb3-754ddb37a812"), 3 },
+                    { new Guid("c6fba3c6-dca7-4fa8-a003-cfc566faccf5"), "Savings account for our users", new Guid("f9f88d26-3ac9-4248-adb3-754ddb37a812"), 1 },
+                    { new Guid("f34b4fdf-759c-4dba-9112-b9bfb8fbfe08"), "Cash account for our users", new Guid("f9f88d26-3ac9-4248-adb3-754ddb37a811"), 0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_OwnerId",
+                table: "Accounts",
+                column: "OwnerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Owners");
+        }
+    }
+}
